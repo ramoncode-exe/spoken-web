f658f7ddd893e7780e2e5a0313277f39d217d87c
ramoncode-exe <ramonpatrick0906@gmail.com>
2025-06-08 18:22:27 +0100

    img width

--- src/components/AboutSpoken.jsx
+++ src/components/AboutSpoken.jsx
@@ -7,7 +7,7 @@ export default function About(){
         <motion.main>
             <div className="flex items-center justify-center mt-3 gap-5 p-1.5 max-lg:flex-wrap font-poppins">
                 <div>
-                    <img className="max-w-[500px]" src={AbtSpoken} alt="About Spoken Img" />
+                    <img className="max-w-500" src={AbtSpoken} alt="About Spoken Img" />
                 </div>
                 <div className="max-w-[500px] mb-10">
                     <h1 className="text-[24px] font-bold mb-3">Spoken: <span className="text-[#9b18d4]">Inglês para Todos,</span> Sem Barreiras</h1>

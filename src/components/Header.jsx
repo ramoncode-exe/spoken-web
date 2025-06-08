a742e9e36a9753f8634a43f61de8347cb679fff2
ramoncode-exe <ramonpatrick0906@gmail.com>
2025-06-08 08:45:00 +0100

    Some changes, new section, improving ux and ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import { motion, scale } from "framer-motion"
-import Logo from "../assets/logo.png"
+import Logo from "../assets/logo.svg"
 
 export default function Header(){
     
     return(
-        <nav className="p-1.5 flex items-center justify-evenly border-b">
+        <nav className="p-1.5 flex items-center justify-evenly border-b font-poppins">
             <motion.div initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 2, ease:"easeOut" }} >
-                <img className="w-36" src={Logo} alt="logo spoken" />
+                <img className="w-[180px]" src={Logo} alt="logo spoken" />
             </motion.div>
             <motion.div className="max-md:hidden" initial={{ x: +200, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 2, ease:"easeOut" }} >
                 <ul className="flex gap-5 items-center text-sm">
@@ -15,9 +15,9 @@ export default function Header(){
                     <motion.li whileHover={{ scale: 1.1 }} className="hover:font-bold p-1">Criador</motion.li>
                 </ul>
             </motion.div>
-            <motion.div className="max-md:hidden" initial={{ x: +200, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 2, ease:"easeOut" }}>
-                <motion.button whileHover={{ scale: 1.1 }} className="bg-[#521689] p-2 rounded-3xl border mr-2 text-[#FFFFFF] hover:bg-[#7f39c1]">Aprender</motion.button>
-                <motion.button whileHover={{ scale: 1.1 }} className="bg-[#521689] p-2 rounded-3xl border text-[#FFFFFF] hover:bg-[#7f39c1]">Meu Canal</motion.button>
+            <motion.div className="max-md:hidden text-sm" initial={{ x: +200, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 2, ease:"easeOut" }}>
+                <motion.button whileHover={{ scale: 1.1 }} className="bg-[#d9d9d9] p-2 rounded-3xl border mr-2 text-[#000000] hover:bg-[#c8c8c8]">Aprender</motion.button>
+                <motion.button whileHover={{ scale: 1.1 }} className="bg-[#9b18d4] p-2 rounded-3xl border text-[#FFFFFF] hover:bg-[#521689]">Meu Canal</motion.button>
             </motion.div>
         </nav>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import { motion } from "framer-motion"
-import BgHome from "../assets/home-bg.png"
+import BgHome from "../assets/home-bg.svg"
 
 export default function Home(){
     return(
-        <main className="flex items-center p-1.5 w-11/12 mx-auto max-lg:flex-wrap-reverse">
+        <main className="flex items-center p-1.5 w-11/12 mx-auto max-lg:flex-wrap-reverse font-poppins">
             <motion.div className="mb-8" initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 2, ease:"easeOut" }}>
-                <span className="p-2 border rounded-lg">Spoken, o seu melhor amigo!</span>
-                <h1 className="text-4xl font-bold mt-3 w-8/12"> Porque Não Falar <span>Inglês Ainda Hoje?</span></h1>
+                <span className="p-2 border rounded-lg text-[14px] border-[#9b18d4] bg-[#9b18d420] ">Spoken, o seu melhor amigo!</span>
+                <h1 className="text-4xl font-bold mt-5 w-8/12"> Porque Não Falar <span className="text-[#9b18d4]">Inglês Ainda Hoje?</span></h1>
                 <p className="w-8/12 mt-3">Com o Spoken, você vai aprender apenas o essencial. O Spoken foca apenas nas 3000 palavras mais faladas do inglês!</p>
             </motion.div>
             <motion.div className="mt-8 mb-8" initial={{ x: +200, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 2, ease:"easeOut" }}>

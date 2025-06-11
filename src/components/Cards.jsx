89a20cb4ed91a22359518947d79d5f1cc13fd658
ramoncode-exe <ramonpatrick0906@gmail.com>
2025-06-11 21:50:49 +0100

    Added Img Background Cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,15 +4,15 @@ import { i, title } from "framer-motion/client"
 export default function Cards(){
 
     const cards = [
-        { title: "Me Apresentando", phrase: "Fale seu nome, idade e de onde é." },
-        { title: "Oi e Tchau", phrase: "Cumprimente e se despeça." },
-        { title: "Minha Rotina", phrase: "Fale sobre o seu dia a dia." },
-        { title: "Comer Fora", phrase: "Peça comida e bebidas." },
-        { title: "Onde Fica?", phrase: "Pergunte e entenda direções."},
-        { title: "Bate-papo", phrase: "Converse de forma simples." },
-        { title: "Compras", phrase: "Fale sobre preços e produtos."},
-        { title: "Emergência", phrase: "Peça ajuda em situações urgentes."},
-        { title: "Viajando", phrase: "Use inglês em aeroportos e hotéis."}
+        {image: "./apresentation.svg"},
+        {image: "./hi-and-bye.svg"},
+        {image: "./routine.svg"},
+        {image: "./eat-out.svg"},
+        {image: "./where-is-it.svg"},
+        {image: "./talking.svg"},
+        {image: "./buy.svg"},
+        {image: "./emergency.svg"},
+        {image: "./travelling.svg"}
     ]
 
     return(
@@ -22,9 +22,7 @@ export default function Cards(){
             </div>
             <div className="flex flex-wrap justify-center items-center mx-auto gap-10 text-center p-1.5 mb-5 mt-5 max-w-[1280px]">
                 {cards.map((card, i) => (
-                    <motion.div key={i} whileHover={{ rotateY: 360, transition: {duration: 0.3 } }} className="bg-gradient-to-t from-[#9b18d420] border border-[#9b18d4] h-[120px] p-1.5 w-[400px] rounded-lg ">
-                        <h1>{card.title}</h1>
-                        <p>{card.phrase}</p>
+                    <motion.div style={{ backgroundImage: `url(${card.image})`, transformStyle: "preserve-3d"}}  key={i} whileHover={{ rotateY: 360, transition: {duration: 0.4 } }} className="bg-gradient-to-t from-[#9b18d420] border border-[#9b18d4] h-[600px] p-1.5 w-[400px] rounded-lg bg-center bg-cover">
                     </motion.div>
                 ))}
                 
